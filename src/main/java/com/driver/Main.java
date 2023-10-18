791e192cc211d4f2886fac963bd7305a12f9f6f3
NikulPatel4693 <121799515+NikulPatel4693@users.noreply.github.com>
2023-10-18 22:52:41 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,11 +6,16 @@ public class A{
   }
 }
 
-
+public class B extends A{
+  @override
+  public String meth(){
+    return "Method is overridden in Extendend class B";
+  }
+}
 
 public class Main {
   public static void main(String[] args){
-    B obj = new B();
+    A obj = new B();
     System.out.println(obj.meth());
   }
 }

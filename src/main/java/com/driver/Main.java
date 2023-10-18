46410c82ad762c85c5b3db2cd5a79c635729d6c6
NikulPatel4693 <121799515+NikulPatel4693@users.noreply.github.com>
2023-10-18 22:50:53 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
+public class A{
+  public String meth(){
+    return "Invoking method from class A";
+  }
+}
+
+
+
 public class Main {
   public static void main(String[] args){
     B obj = new B();
